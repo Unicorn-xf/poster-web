536ddc41b53279c26999e5e9256181b696dd717b
xiongfeng <2318452499@qq.com>
2021-07-05 21:21:00 +0800

    增加模板下载功能

--- src/view/poster/poster-produce/posterProduce.vue
+++ src/view/poster/poster-produce/posterProduce.vue
@@ -8,10 +8,10 @@
     </Row>
     <Card title="导入EXCEL">
       <Row>
-        <Upload action="" :before-upload="handleBeforeUpload" accept=".xls, .xlsx">
+        <Upload action="" :before-upload="handleBeforeUpload" accept=".xls, .xlsx" style="display: inline-block;">
           <Button icon="ios-cloud-upload-outline" :loading="uploadLoading" @click="handleUploadFile">上传文件</Button>
-          <Button icon="md-download" :loading="exportLoading" @click="exportExcel" style="margin-left: 30px;">下载模板</Button>
         </Upload>
+          <Button icon="md-download"  @click="exportExcel" style="margin-left: 30px;">下载模板</Button>
       </Row>
       <Row>
         <div class="ivu-upload-list-file" v-if="file !== null">
@@ -112,23 +112,85 @@ export default {
         const tableTitle = header.map(item => { return { title: item, key: item } })
         this.tableData = results
         this.tableTitle = tableTitle
+        console.info("读取的数据："+JSON.stringify(results))
+        console.info("头部："+JSON.stringify(tableTitle))
         this.uploadLoading = false
         this.tableLoading = false
         this.showRemoveFile = true
       }
     },
     exportExcel () {
-        this.exportLoading = true
-        const params = {
-          title: ['姓名', '单位名称', '产品名称','保费'],
-          key: ['name', 'danweiName', 'changpinName','baofei'],
-          data: this.tableDataInfo,
-          autoWidth: true,
-          filename: '海报模板'
-        }
-        excel.export_array_to_excel(params)
-        this.exportLoading = false
-    }
+        // this.exportLoading = true
+        // const params = {
+        //   title: ['姓名', '单位名称', '产品名称','保费'],
+        //   key: ['name', 'danweiName', 'changpinName','baofei'],
+        //   data: this.tableDataInfo,
+        //   autoWidth: true,
+        //   filename: '海报模板'
+        // }
+        // excel.export_array_to_excel(params)
+        // this.exportLoading = false
+        // 规范（隐藏）
+        let dataArray = ["userName","unit","productName","premium"]
+        // 表头
+        let titleArray = ["姓名","单位","产品名称","保费"]
+        // 列宽
+        let colsWidth = [{wch:10},{wch:10},{wch:10},{wch:10}]
+
+        var sheet = xlsx.utils.aoa_to_sheet([dataArray, titleArray]);
+        // 隐藏规范数据
+        sheet['!rows'] = [{'hidden': true}]
+        // 调整列宽
+        sheet['!cols'] = colsWidth
+        this.openDownloadDialog(this.sheet2blob(sheet), '数据模板.xlsx');
+    },
+    /**
+     * 通用的打开下载对话框方法，没有测试过具体兼容性
+     * @param url 下载地址，也可以是一个blob对象，必选
+     * @param saveName 保存文件名，可选
+     */
+    openDownloadDialog: function(url, saveName) {
+      if(typeof url == 'object' && url instanceof Blob)
+      {
+        url = URL.createObjectURL(url); // 创建blob地址
+      }
+      var aLink = document.createElement('a');
+      aLink.href = url;
+      aLink.download = saveName || ''; // HTML5新增的属性，指定保存文件名，可以不要后缀，注意，file:///模式下不会生效
+      var event;
+      if(window.MouseEvent) event = new MouseEvent('click');
+      else
+      {
+        event = document.createEvent('MouseEvents');
+        event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+      }
+      aLink.dispatchEvent(event);
+    },
+    // 将一个sheet转成最终的excel文件的blob对象，然后利用URL.createObjectURL下载
+    sheet2blob: function(sheet, sheetName) {
+      sheetName = sheetName || 'sheet1';
+      var workbook = {
+        SheetNames: [sheetName],
+        Sheets: {}
+      };
+      workbook.Sheets[sheetName] = sheet;
+      // 生成excel的配置项
+      var wopts = {
+        bookType: 'xlsx', // 要生成的文件类型
+        bookSST: false, // 是否生成Shared String Table，官方解释是，如果开启生成速度会下降，但在低版本IOS设备上有更好的兼容性
+        type: 'binary'
+      };
+      var wbout = xlsx.write(workbook, wopts);
+      var blob = new Blob([s2ab(wbout)], {type:"application/octet-stream"});
+      // 字符串转ArrayBuffer
+      function s2ab(s) {
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+        return buf;
+      }
+      return blob;
+    },
   },
   created () {
 
